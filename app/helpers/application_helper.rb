18c809b9fd12cb850fc93a8b284280d3d5e3a540
hct97 <trunghuynh060597@gmail.com>
2018-10-26 08:29:07 +0700

    Rails-flavored Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 module ApplicationHelper
-
+  
+  def full_title page_title = ""
+    base_title = t "static_pages.rails_tutorial"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end
